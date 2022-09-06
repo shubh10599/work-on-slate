5746b46573cb742f77d76656eb31ae8192ddfdd1
Nisarg Patel <nhp0640@gmail.com>
2022-09-06 09:34:53 +0530

    code formatting and removed some tools

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import "./App.css";
 // import RichTextExample from "./component/editor";
 import QuillEditor from "./component/QuillEditor";
-import 'react-quill/dist/quill.snow.css';
+import "react-quill/dist/quill.snow.css";
 
 const App = () => {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState("<h3>Hello, <i>Nisarg</i></h3>");
 
   return (
     <>
-      <QuillEditor handleChange={setValue} value={value}/>
+      <QuillEditor handleChange={setValue} value={value} />
       <button onClick={() => console.log(value)}>Submit</button>
       {/* <LinkedInIcon /> */}
     </>
--- src/component/QuillEditor.js
+++ src/component/QuillEditor.js
@@ -3,61 +3,71 @@ import ReactQuill, { Quill } from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
 const QuillEditor = ({ handleChange, value }) => {
-//   // let editor = new Quill('.editor');
-//   // let option  = {
-//   //   debug: 'info',
-//   //   modules: {
-//   //     toolbar: '#toolbar'
-//   //   },
-//   //   placeholder: 'Compose an epic...',
-//   //   readOnly: true,
-//   //   theme: 'snow'
-//   // };
-//   return( 
-//   <ReactQuill theme="snow" value={value} onChange={handleChange} />
-//   );
-// };
+  //   // let editor = new Quill('.editor');
+  //   // let option  = {
+  //   //   debug: 'info',
+  //   //   modules: {
+  //   //     toolbar: '#toolbar'
+  //   //   },
+  //   //   placeholder: 'Compose an epic...',
+  //   //   readOnly: true,
+  //   //   theme: 'snow'
+  //   // };
+  //   return(
+  //   <ReactQuill theme="snow" value={value} onChange={handleChange} />
+  //   );
+  // };
 
-// export default QuillEditor;
+  // export default QuillEditor;
 
-// class MyComponent extends Component {
+  // class MyComponent extends Component {
   // constructor(props) {
-    // super(props);
-    // this.state = {
-      // text: "",
-    // }
+  // super(props);
+  // this.state = {
+  // text: "",
+  // }
   // }
 
   const modules = {
     toolbar: [
-      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-      ['bold', 'italic', 'underline','strike', 'blockquote'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-      ['link', 'image'],
-      ['clean']
+      // [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link"],
+      // ["clean", "image"],
     ],
-  }
+  };
 
   const formats = [
-    'header',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image'
+    // "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    // "image",
   ];
 
-  
-    return (
-      <div className="text-editor">
-        <ReactQuill theme="snow"
-                    modules={modules}
-                    formats={formats}
-                    value={value}
-                    onChange={handleChange}
-                    >
-        </ReactQuill>
-      </div>
-    )
-  
-}
+  return (
+    <div className="text-editor">
+      <ReactQuill
+        theme="snow"
+        modules={modules}
+        formats={formats}
+        value={value}
+        onChange={handleChange}
+      ></ReactQuill>
+    </div>
+  );
+};
 
 export default QuillEditor;
